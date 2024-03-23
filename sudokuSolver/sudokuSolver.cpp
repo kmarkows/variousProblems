b16f57171b14c05cc84c6f36187a8179318ed599
Konrad Markowski <konradmarkowski@Konrads-MacBook-Air.local>
2024-03-23 17:25:32 +0100

    removal of code which does not work

--- sudokuSolver/sudokuSolver.cpp
+++ sudokuSolver/sudokuSolver.cpp
@@ -47,7 +47,103 @@ void SudokuSolver::saveSolvedSudokuToFile()
 
 void SudokuSolver::solve()
 {
+    setInitialConditions();
+
+    std::cout << " --- rowsBits ---" << std::endl;
+    for (const auto rowBits : rowsBits)
+    {
+        std::cout << rowBits.to_string() << std::endl;
+    }
+
+    std::cout << " --- colsBits ---" << std::endl;
+    for (const auto colBits : columnsBits)
+    {
+        std::cout << colBits.to_string() << std::endl;
+    }
+
+    bool isSudokuSolved = solveRecurrsively(0, 0);
+    print();
     saveSolvedSudokuToFile();
+}
+
+bool SudokuSolver::solveRecurrsively(uint8_t startRow, uint8_t startCol)
+{
+    std::pair<uint8_t, uint8_t> nextEmptyPosition{getNextEmptyPosition(startRow, startCol)};
+    std::cout << (int)nextEmptyPosition.first << " " << (int)nextEmptyPosition.second << std::endl;
+
+    if (nextEmptyPosition.first == sudokuSize)
+    {
+        return true;
+    }
+
+    std::bitset<sudokuSize> currentPosBits = rowsBits[nextEmptyPosition.first] |
+        columnsBits[nextEmptyPosition.second] | cellsBits[getCellId(nextEmptyPosition.first, nextEmptyPosition.second)];
+
+    if (currentPosBits.all())
+    {
+        return false;
+    }
+
+    std::cout << currentPosBits.to_string() << std::endl;
+
+    for (uint8_t bitId = 0; bitId < sudokuSize; bitId++)
+    {
+        if (not currentPosBits[bitId])
+        {
+            sudoku[nextEmptyPosition.first][nextEmptyPosition.second] = bitId + 1;
+            rowsBits[nextEmptyPosition.first].set(bitId);
+            columnsBits[nextEmptyPosition.second].set(bitId);
+            cellsBits[getCellId(nextEmptyPosition.first, nextEmptyPosition.second)].set(bitId);
+
+            if (solveRecurrsively(nextEmptyPosition.first, nextEmptyPosition.second))
+            {
+                return true;
+            }
+
+            rowsBits[nextEmptyPosition.first].reset(bitId);
+            columnsBits[nextEmptyPosition.second].reset(bitId);
+            cellsBits[getCellId(nextEmptyPosition.first, nextEmptyPosition.second)].reset(bitId);
+        }
+    }
+
+    sudoku[nextEmptyPosition.first][nextEmptyPosition.second] = 0;
+    return false;
+}
+
+std::pair<uint8_t, uint8_t> SudokuSolver::getNextEmptyPosition(uint8_t row, uint8_t col)
+{
+    while (row != sudokuSize)
+    {
+        if (sudoku[row][col] == 0)
+        {
+            return {row, col};
+        }
+        row = row + (col + 1) / 9;
+        col = (col + 1) % 9;
+    }
+    return {sudokuSize, 0};
+}
+
+uint8_t SudokuSolver::getCellId(const uint8_t row, const uint8_t col)
+{
+    return (row / 3) * 3 + col / 3;
+}
+
+void SudokuSolver::setInitialConditions()
+{
+    for (uint8_t row = 0; row < sudokuSize; row++)
+    {
+        for (uint8_t col = 0; col < sudokuSize; col++)
+        {
+            if (sudoku[row][col] != 0)
+            {
+                std::cout << "row:" << (int)row << " col:" << (int)col << " cell: " << (int)getCellId(row, col) << " number:" << (int)sudoku[row][col] << std::endl;
+                rowsBits[row].set(sudoku[row][col] - 1);
+                columnsBits[col].set(sudoku[row][col] - 1);
+                cellsBits[getCellId(row, col)].set(sudoku[row][col] - 1);
+            }
+        }
+    }
 }
 
 void SudokuSolver::print()
--- sudokuSolver/sudokuSolver.hpp
+++ sudokuSolver/sudokuSolver.hpp
@@ -4,7 +4,6 @@
 #include <cstdlib>
 #include <array>
 #include <string>
-#include <set>
 
 constexpr uint8_t sudokuSize = 9;
 
@@ -13,8 +12,16 @@ class SudokuSolver
 private:
     void readUnsolvedSudokuFromFile();
     void saveSolvedSudokuToFile();
+    bool solveRecurrsively(uint8_t startRow, uint8_t startCol);
+
+    uint8_t getCellId(const uint8_t row, const uint8_t col);
+    std::pair<uint8_t, uint8_t> getNextEmptyPosition(uint8_t row, uint8_t col);
+    void setInitialConditions();
 
     std::array<std::array<uint8_t, sudokuSize>, sudokuSize> sudoku{};
+    std::array<std::bitset<sudokuSize>, sudokuSize> rowsBits{0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::array<std::bitset<sudokuSize>, sudokuSize> columnsBits{0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::array<std::bitset<sudokuSize>, sudokuSize> cellsBits{0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 public:
     SudokuSolver();
